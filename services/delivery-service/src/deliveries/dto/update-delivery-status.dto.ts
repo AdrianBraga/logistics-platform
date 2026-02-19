99de90669985f3e489c002f989e374138ceb2336
Adrian Braga <adrian.braga.ro@gmail.com>
2026-02-18 22:28:08 -0400

    docs(delivery): setup swagger api documentation and decorators

--- services/delivery-service/src/deliveries/dto/update-delivery-status.dto.ts
+++ services/delivery-service/src/deliveries/dto/update-delivery-status.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 import { DeliveryStatus } from 'src/enums/delivery-status.enum';
 
 export class UpdateDeliveryStatusDto {
+  @ApiProperty({
+    description: 'The status of the delivery',
+    enum: DeliveryStatus,
+    example: DeliveryStatus.IN_TRANSIT,
+  })
   @IsEnum(DeliveryStatus, {
     message: 'Invalid status. Use: CREATED, IN_TRANSIT, DELIVERED ou CANCELLED',
   })
